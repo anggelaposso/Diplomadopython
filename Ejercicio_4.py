66874e9cd5f1257e88b7e623e64006613daa67a8
anggelaposso <80720741+anggelaposso@users.noreply.github.com>
2022-05-28 14:32:34 -0500

    Ejercicio #4 modulo_2

---
+++ Ejercicio_4.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 28 13:48:50 2022
+
+@author: Angela Posso
+"""
+("a", "b", "c", "d", "e", "f")
+
+a=1 
+b=2
+c=3
+d=4
+e=5
+f=6
+
+ecu1 = (a+(b/c))/(d+(e/f))
+ecu2 = a-(b/(c-d))
+
+ecu3 = ecu1
+ecu1 = ecu2
+ecu2 = ecu3
+
+print(ecu1)
+print(ecu2)
