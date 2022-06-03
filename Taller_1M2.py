a38a05bfb8012172aeddab5e8312f509991ef6c8
anggelaposso <80720741+anggelaposso@users.noreply.github.com>
2022-06-03 16:46:21 -0500

    Taller ecuaciones modulo #2

---
+++ Taller_1M2.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Creado el sáb 28 mayo 14:33:27 2022
+
+@author: Angela Posso
+"""
+
+"""
+correo: angela.posso@upb.edu.co
+Id: 502224
+"""
+
+a = 1
+b = 2
+c = 3
+d = 4
+d = 5
+e = 6
+f = 7
+
+
+ecu1  = ( a  + ( b / c )) / ( d + ( e / f ))
+print ( "Valor de la formula uno es: " )
+imprimir ( ecu1 )
+
+
+ecu2  =  a  - ( b / ( c - d ))
+print ( "Valor de la formula dos es: " )
+imprimir ( ecu2 )
+
+
+
+imprimir ( "Ecu1: " , ecu1 , "Ecu2: " , ecu2 , ")
+temperatura  =  ecu1
+ecu1  =  ecu2
+ecu2  =  temperatura
+print ( "Valores intercambiados" )
+imprimir ( "Ecu1: " , ecu1 , "Ecu2: " , ecu2 )
